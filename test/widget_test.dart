0b69549e7bf0755286337ca6b1ae1e4cd350a25b
Osama Elkot <osama.elkot8@gmail.com>
2022-04-04 23:11:38 +0200

    - widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,117 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:garden/main.dart';
+import 'package:garden/models/entities/plant/plant.dart';
+import 'package:garden/models/utilities/extensions/datetime_extension.dart';
+import 'package:garden/ui/components/buttons/default_text_button.dart';
+import 'package:garden/ui/components/custom_single_child_scroll_view.dart';
+import 'package:garden/ui/components/drop_downs/default_drop_down.dart';
+import 'package:garden/ui/components/refetch_view.dart';
+import 'package:garden/ui/components/text_fields/default_text_field.dart';
+import 'package:garden/ui/screens/home/plant/plant_manager/planting_date_view.dart';
+import 'package:garden/ui/screens/home/plant/plants_screen/plants_list_view/plant_view.dart';
+import 'package:garden/ui/screens/home/plant/plants_screen/plants_list_view/plants_list_view.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('PlantView test', (WidgetTester tester) async {
+    final Plant _plant = Plant(
+        id: 1,
+        name: 'Test Plant',
+        type: 'alpines',
+        date: DateTime.now().defaultFormat());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: PlantView(plant: _plant),
+      ),
+    ));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    expect(find.text(_plant.name), findsOneWidget);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('PlantsListView test', (WidgetTester tester) async {
+    final Plant _plantA = Plant(
+        id: 1,
+        name: 'Test Plant A',
+        type: 'alpines',
+        date: DateTime.now().defaultFormat());
+    final Plant _plantB = Plant(
+        id: 2,
+        name: 'Test Plant B',
+        type: 'bulbs',
+        date: DateTime.now().defaultFormat());
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: PlantsListView(plants: [_plantA, _plantB]),
+      ),
+    ));
+
+    expect(find.text(_plantA.type), findsOneWidget);
+    expect(find.text(_plantB.type), findsOneWidget);
+  });
+
+  testWidgets('PlantingDateView test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: PlantingDateView(value: DateTime.now(), onEdit: () {}),
+      ),
+    ));
+
+    expect(find.byIcon(Icons.edit), findsOneWidget);
+  });
+
+  testWidgets('DefaultTextButton test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: DefaultTextButton(text: 'Save'),
+      ),
+    ));
+
+    expect(find.text('Save'), findsOneWidget);
+  });
+
+  testWidgets('DefaultDropDown test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: DefaultDropDown<String>(
+          value: 'Test value',
+          hint: 'Test Hint',
+        ),
+      ),
+    ));
+
+    expect(find.text('Test Hint'), findsOneWidget);
+  });
+
+  testWidgets('DefaultTextField test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: DefaultTextField(
+          prefixIcon: Icon(Icons.search),
+        ),
+      ),
+    ));
+
+    expect(find.byIcon(Icons.search), findsOneWidget);
+  });
+
+  testWidgets('CustomSingleChildScrollView test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: CustomSingleChildScrollView(child: Text('Test Value')),
+      ),
+    ));
+
+    expect(find.text('Test Value'), findsOneWidget);
+  });
+
+  testWidgets('ReFetchView test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(
+      home: Scaffold(
+        body: ReFetchView(error: 'error'),
+      ),
+    ));
+
+    expect(find.text('error'), findsOneWidget);
   });
 }
